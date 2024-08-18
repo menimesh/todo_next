8a6f5349e014db59394769e41946d6622bc9ebdf
menimesh <nimeshkhatiwadaa@gmail.com>
2024-08-18 16:13:30 +0545

    todoproject on next

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,5 +1,16 @@
-import { asyncError } from "../../../middlewares/error";
+import { connectDB } from "../../../utils/features";
+import { asyncError, errorHandler } from "../../../middlewares/error";
+import {User} from "../../../models/user"
 const handler = asyncError(async (req,res)=>{
     const {name, email, password} = req.body;
+    if(!name || !email || !password) return errorHandler(res,400,"Please enter all the field")
+        await connectDB();
+    const user = await User.findOne({email});
+    if(user) return errorHandler(res,400,"User registered with this Email");
+    await User.create({
+        name,
+        email,
+        password,
+    })
 });
  export default handler
