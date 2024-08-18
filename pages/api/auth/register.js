ee06b9e079ee9f74c9000dbec293692b2fa8d536
menimesh <nimeshkhatiwadaa@gmail.com>
2024-08-18 16:26:57 +0545

    todoproject on next

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,6 +1,7 @@
-import { connectDB } from "../../../utils/features";
+import { connectDB, cookieSetter } from "../../../utils/features";
 import { asyncError, errorHandler } from "../../../middlewares/error";
 import {User} from "../../../models/user"
+import { serialize } from "cookie";
 const handler = asyncError(async (req,res)=>{
     const {name, email, password} = req.body;
     if(!name || !email || !password) return errorHandler(res,400,"Please enter all the field")
@@ -11,6 +12,13 @@ const handler = asyncError(async (req,res)=>{
         name,
         email,
         password,
-    })
+    });
+ const token = "nimesh";
+ cookieSetter(res,token,true)
+ res.status(201).json({
+    sucess:true,
+    message:"user created sucessfully"
+ })
+
 });
  export default handler
--- utils/features.js
+++ utils/features.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
+import { serialize } from "cookie";
 export const connectDB = async() =>{
 const {connection} = await mongoose.connect(process.env.MONGO_URI,{
     dbName:"NextTodo",
 });
 console.log(`DataBase connected on ${connection.host}`)
 };
+export const cookieSetter = (res,token,set)=>{
+    const token ="nimesh"
+     res.setHeader("Set-Cookie",serialize("token",token,{
+        path:"/",
+        httpOnly:true,
+        maxAge:set ? 60*60*1000 :0,
+     }))
+}
